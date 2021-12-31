5fc9a1d82765cd3ff30727019bac824b5349c3b1
최재강 <chl8273@naver.com>
2021-12-31 12:38:43 +0900

    change path to use tmp

--- tvm/python/tvm/contrib/download.py
+++ tvm/python/tvm/contrib/download.py
@@ -132,7 +132,7 @@ def download(url, path, overwrite=False, size_compare=False, retries=3):
 if "TEST_DATA_ROOT_PATH" in os.environ:
     TEST_DATA_ROOT_PATH = Path(os.environ.get("TEST_DATA_ROOT_PATH"))
 else:
-    TEST_DATA_ROOT_PATH = Path(Path("~").expanduser(), ".tvm_test_data")
+    TEST_DATA_ROOT_PATH = Path("/tmp", ".tvm_test_data")
 TEST_DATA_ROOT_PATH.mkdir(parents=True, exist_ok=True)
 
 
